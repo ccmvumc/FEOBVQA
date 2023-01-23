a03ec55d06c2232db2eb23a81ae0446ac0b2077a
bud42 <bdboyd42@gmail.com>
2023-01-23 15:07:00 -0500

    Update docker_test.sh

--- docker/docker_test.sh
+++ docker/docker_test.sh
@@ -23,6 +23,9 @@ curl -s -k -n  \
 "https://xnat.vanderbilt.edu/xnat/data/projects/DSCHOL/subjects/DST3050001/experiments/DST3050001_MR1/assessors/DSCHOL-x-DST3050001-x-DST3050001_MR1-x-FS7_v1-x-00d5341d/out/resources/SUBJ/files/mri/wmparc.mgz" \
 -o "$HOME/TEST-FEOBVQA/INPUTS/wmparc.mgz"
 
+curl -s -k -n  \
+"https://xnat.vanderbilt.edu/xnat/data/projects/DSCHOL/subjects/DST3050001/experiments/DST3050001_PET2/scans/7305/resources/NIFTI/files/7305__research_only_BR_DY1mm_CTAC_1mm__Brain_Dynamic.nii.gz" \
+-o "$HOME/TEST-FEOBVQA/INPUTS/PET.nii.gz"
 
 echo "Running docker"
 docker run \
