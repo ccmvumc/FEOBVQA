eb5e74e1033f389d6063bab60778d97e1c54e66f
bud42 <bdboyd42@gmail.com>
2024-06-20 21:25:13 -0400

    Update pre.sh

--- src/DSCHOL/pre.sh
+++ src/DSCHOL/pre.sh
@@ -1,12 +1,14 @@
 set -x
 
+ls -ltrR /INPUTS
+
 mkdir -p /OUTPUTS/DATA/SUBJECTS
 
 cd /INPUTS
 for i in *
 do
   # Copy from inputs to outputs
-  cp -r /INPUTS/$i/*/*/SUBJ /OUTPUTS/DATA/SUBJECTS/$i
+  cp -r /INPUTS/$i/*/SUBJ /OUTPUTS/DATA/SUBJECTS/$i
 
   # Append to subject list
   echo $i >> /OUTPUTS/subjects.txt
