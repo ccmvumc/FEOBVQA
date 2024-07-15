d18b3f94033caaa467f9b7647a28b062f6195c93
bud42 <bdboyd42@gmail.com>
2024-07-15 18:35:05 -0400

    updates for pib summary

---
+++ src/pib_summary/main.sh
@@ -0,0 +1,29 @@
+set -x
+
+mkdir /OUTPUTS/DATA
+    
+cd /INPUTS
+
+for i in *;do
+
+    echo $i
+
+    mkdir -p /OUTPUTS/DATA/$i
+
+    cd /OUTPUTS/DATA/$i
+
+    cp /INPUTS/$i/gtmpvc.cblmgmwm.output/gtm.stats.dat .
+
+    # Make file per column
+    yes $i | head -n 100 > subject.txt
+    yes cblmgmwm | head -n 100 > ref.txt
+    yes gtm | head -n 100 > gtm.txt
+    awk '{print $3}' gtm.stats.dat > labels.txt
+    awk '{print $7}' gtm.stats.dat > gtm.suvr
+
+    # Combine into single file
+    paste subject.txt ref.txt gtm.txt labels.txt gtm.suvr >> all.suvr
+
+done
+
+cat /OUTPUTS/DATA/*/all.suvr > /OUTPUTS/suvr.txt
